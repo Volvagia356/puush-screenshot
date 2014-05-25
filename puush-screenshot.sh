51e8bbfc3836c2c78b199fc20f5de2e36fcb67a6
Raymond Choo <volvagia356@gmail.com>
2014-05-25 12:10:30 -0700

    Added arguments to use specific modes so zenity is now optional.

--- puush-screenshot.sh
+++ puush-screenshot.sh
@@ -7,9 +7,35 @@
 #Insert your API key here
 APIKEY=""
 
+function zenity {
+    if [ $USE_ZENITY = 0 ]; then
+        return 0
+    else
+        env zenity "$@"
+        return $?
+    fi
+}
+
 FILENAME=/tmp/`tr -dc A-Za-z0-9 < /dev/urandom | head -c 8`.png
 
-TYPE=`zenity --list --text="Select a screenshot type:" --column="" --column="Type" --hide-header --hide-column=1 1 "Entire desktop" 2 "Single window or rectangular region"`
+if [ $# = 0 ]; then
+    USE_ZENITY=1
+    TYPE=`zenity --list --text="Select a screenshot type:" --column="" --column="Type" --hide-header --hide-column=1 1 "Entire desktop" 2 "Single window or rectangular region"`
+elif [ $# = 1 ]; then
+    USE_ZENITY=0
+    if [ $1 = "--full" ]; then
+        TYPE=1
+    elif [ $1 = "--area" ]; then
+        TYPE=2
+    else
+        echo "Invalid argument!"
+        exit
+    fi
+else
+    echo "Invalid argument!"
+    exit
+fi
+
 if [ $? != 0 ]; then
     exit
 elif [ $TYPE = 1 ]; then
@@ -21,14 +47,17 @@ fi
 HTML='<img src="data:image/png;base64,'`base64 $FILENAME`'" width="200px"><p>Upload this image?'
 echo $HTML | zenity --text-info --html --filename=/dev/stdin
 if [ $? = 0 ]; then
-    RESPONSE=`curl -F "k=$APIKEY" -F "z=poop" -F "f=@$FILENAME" https://puush.me/api/up`
-    echo $RESPONSE
+    RESPONSE=`curl -s -F "k=$APIKEY" -F "z=poop" -F "f=@$FILENAME" https://puush.me/api/up`
     if [ $? != 0 ]; then
         zenity --error
     else
         ARGS=(`echo $RESPONSE | tr ',' ' '`)
-        echo -n ${ARGS[1]} | xclip -sel clip
-        zenity --info --text="URL copied to clipboard."
+        if [ ${ARGS[0]} = 0 ]; then
+            echo -n ${ARGS[1]} | xclip -sel clip
+            zenity --info --text="URL copied to clipboard."
+        else
+          zenity --error
+        fi
     fi
 fi
 rm $FILENAME
