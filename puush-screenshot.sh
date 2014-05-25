4a410542dcd4f4e3e3c8e0dbd2495e9ca9a76978
Raymond Choo <volvagia356@gmail.com>
2014-05-25 03:15:49 -0700

    Initial commit!

---
+++ puush-screenshot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Screen capture and upload utility for puush
+# (C) 2014 Volvagia356
+#
+# Requires zenity and imagemagick
+
+#Insert your API key here
+APIKEY=""
+
+FILENAME=/tmp/`tr -dc A-Za-z0-9 < /dev/urandom | head -c 8`.png
+
+TYPE=`zenity --list --text="Select a screenshot type:" --column="" --column="Type" --hide-header --hide-column=1 1 "Entire desktop" 2 "Single window or rectangular region"`
+if [ $? != 0 ]; then
+    exit
+elif [ $TYPE = 1 ]; then
+    import -window root $FILENAME
+elif [ $TYPE = 2 ]; then
+    import $FILENAME
+fi
+
+HTML='<img src="data:image/png;base64,'`base64 $FILENAME`'" width="200px"><p>Upload this image?'
+echo $HTML | zenity --text-info --html --filename=/dev/stdin
+if [ $? = 0 ]; then
+    RESPONSE=`curl -F "k=$APIKEY" -F "z=poop" -F "f=@$FILENAME" https://puush.me/api/up`
+    echo $RESPONSE
+    if [ $? != 0 ]; then
+        zenity --error
+    else
+        ARGS=(`echo $RESPONSE | tr ',' ' '`)
+        echo -n ${ARGS[1]} | xclip -sel clip
+        zenity --info --text="URL copied to clipboard."
+    fi
+fi
+rm $FILENAME
